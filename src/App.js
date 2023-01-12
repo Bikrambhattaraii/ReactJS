9ee3c2af179d9e30642b7cedbcafebb65a5794ab
bikram bhattarai <bikrambhattarai296@gmail.com>
2023-01-12 12:31:32 +0545

    react 2

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import Clear from './Clear';
+import React from 'react';
+
+function App() {
+  return (
+    <Clear></Clear>
+  );
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,6 @@
+export const data = [
+    { id: 1, name: 'jiten' },
+    { id: 2, name: 'bikram' },
+    { id: 3, name: 'bibek' },
+    { id: 4, name: 'binit' },
+  ];
